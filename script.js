dc9a3d7c1aa6b8b58d418ced87d63dcc4a5bc218
Dan Harrison <dan@wombatlabs.ai>
2026-01-03 15:02:43 -0800

    Update wrangler.tomo to use index.js as main

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-// for github/cloudflare
-import html from './index.html';
-export default {
-  async fetch(request) {
-    return new Response(html, {
-      headers: { 'content-type': 'text/html' },
-    });
-  },
-};
-
-
 // Persona data
 const personas = {
     steve: {
